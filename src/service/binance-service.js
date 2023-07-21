e0fe6b8bab38787283e414a12f47eca4304e8a99
imadusanka <isurumadusanka398@gmail.com>
2023-07-21 21:26:57 +0530

    changed binance api

--- src/service/binance-service.js
+++ src/service/binance-service.js
@@ -4,7 +4,7 @@ import { config } from "../config.js";
 class BinanceService {
   constructor() {
     this.instance = axios.create({
-      baseURL: "https://fapi.binance.com/fapi",
+      baseURL: "https://api.binance.us/api",
     });
     this.interval = config.INTERVAL;
     this.windowSize = config.WINDOW_SIZE;
@@ -13,7 +13,7 @@ class BinanceService {
   async getPriceData(symbol) {
     try {
       const response = await this.instance.get(
-        `/v1/klines?symbol=${symbol}&interval=${this.interval}&limit=${this.windowSize}`,
+        `/v3/klines?symbol=${symbol}&interval=${this.interval}&limit=${this.windowSize}`,
       );
       return response.data;
     } catch (error) {
