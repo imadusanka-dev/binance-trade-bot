ef6d38e81384338a4be1dffad335532fe211c245
imadusanka <isurumadusanka398@gmail.com>
2023-07-21 19:46:24 +0530

    added eth and xrp instances

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import "dotenv/config";
-import dayjs from "dayjs";
 import cron from "node-cron";
 import Trader from "./trader.js";
 import { symbols } from "./constants.js";
@@ -9,12 +8,23 @@ import { symbols } from "./constants.js";
 //base currency is the currency to be traded
 //quote currency is the currency to be used to buy base currency
 //example: BTC/USDT
+console.log("-----App Started");
+
 const btcTrader = new Trader(symbols.BTC, symbols.USDT);
+const ethTrader = new Trader(symbols.ETH, symbols.USDT);
+const xrpTrader = new Trader(symbols.XRP, symbols.USDT);
 
 //run trader every 30 mins
 cron.schedule("*/30 * * * *", () => {
-  console.log("-----running trader", dayjs().format("YYYY-MM-DD HH:mm:ss"));
+  console.log(
+    "-----Running Trader",
+    new Date().toLocaleString("en-US", {
+      timeZone: "Asia/Colombo",
+    }),
+  );
 
   //run all trader instances here
   btcTrader.run();
+  ethTrader.run();
+  xrpTrader.run();
 });
