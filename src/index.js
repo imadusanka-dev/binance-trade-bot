e87aceb3565e2acd4958b9ef92ef3ca46691841a
imadusanka <isurumadusanka398@gmail.com>
2023-07-21 20:50:50 +0530

    added express

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import "dotenv/config";
-import cron from "node-cron";
+import express from "express";
 import Trader from "./trader.js";
 import { symbols } from "./constants.js";
+
+const app = express();
 
 //create instance of trader
 //could be more than one (ETH, BTC, BNB etc)
 //base currency is the currency to be traded
 //quote currency is the currency to be used to buy base currency
 //example: BTC/USDT
-console.log("-----App Started");
-
 const btcTrader = new Trader(symbols.BTC, symbols.USDT);
 const ethTrader = new Trader(symbols.ETH, symbols.USDT);
 const xrpTrader = new Trader(symbols.XRP, symbols.USDT);
 
-//run trader every 30 mins
-cron.schedule("*/30 * * * *", () => {
+app.post("/start", (req, res) => {
   console.log(
     "-----Running Trader",
     new Date().toLocaleString("en-US", {
@@ -27,4 +26,12 @@ cron.schedule("*/30 * * * *", () => {
   btcTrader.run();
   ethTrader.run();
   xrpTrader.run();
+
+  res.status(200).json({
+    message: "Trader started",
+  });
+});
+
+app.listen(3000, () => {
+  console.log(`-----Server is running on port 3000`);
 });
