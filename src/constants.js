b3ee7bf44b2fd2688e61ef9227ca5ee60b081a5b
imadusanka <isurumadusanka398@gmail.com>
2023-07-21 19:47:35 +0530

    removed trade types and added new symbols

--- src/constants.js
+++ src/constants.js
@@ -3,19 +3,9 @@ export const tradeSide = {
   SELL: "SELL",
 };
 
-export const tradeTypes = {
-  LIMIT: "LIMIT",
-  MARKET: "MARKET",
-  STOP_LOSS: "STOP_LOSS",
-  STOP_LOSS_LIMIT: "STOP_LOSS_LIMIT",
-  TAKE_PROFIT: "TAKE_PROFIT",
-  TAKE_PROFIT_LIMIT: "TAKE_PROFIT_LIMIT",
-  LIMIT_MAKER: "LIMIT_MAKER",
-};
-
 export const symbols = {
   BTC: "BTC",
   ETH: "ETH",
-  BNB: "BNB",
+  XRP: "XRP",
   USDT: "USDT",
 };
