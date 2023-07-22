33c600d993a40f2dec262a46ffe9036bb9842dfb
imadusanka <isurumadusanka398@gmail.com>
2023-07-22 21:21:30 +0530

    changed bandwidth and interval

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export const config = {
   WINDOW_SIZE: 500, //candles count
-  BANDWIDTH: 8,
+  BANDWIDTH: 13,
   MULTIPLIER: 3,
-  INTERVAL: "30m",
+  INTERVAL: "1h",
   TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
   TELEGRAM_CHAT_ID: process.env.TELEGRAM_CHAT_ID,
 };
